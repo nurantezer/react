f6b096aeeca177be811ee791a3752f72d0c4b65b
nurantezer <nurantezer68@gmail.com>
2023-02-14 22:58:26 +0300

    end

--- 03-props/src/App.js
+++ 03-props/src/App.js
@@ -1,10 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+
+import Person from "./components/Person";
 
 function App() {
   return (
     <div>
-      
+      <Person name="Ali" 
+      img="" 
+      tel="8897787715"/>
     </div>
   );
 }
---
+++ 03-props/src/components/Msg.jsx
@@ -0,0 +1,9 @@
+
+//*proplar prenten childe doğrudur
+const Msg = ({isim}) => {
+  return (
+    <div>Hi, My name is {isim}</div>
+  )
+}
+
+export default Msg
