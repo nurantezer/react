83136bad6ab5f3fa0efefcd293dad279cb84c178
nurantezer <nurantezer68@gmail.com>
2023-02-14 21:54:19 +0300

    style

---
+++ 02-react-style/src/Content.jsx
@@ -0,0 +1,24 @@
+//*public klasörüne koyduklarımı doğrudan kullanabiliyorum.
+import "./Content.css"
+const Content = () => {
+const pStyle = {
+    fontFamily:"Tahoma",
+    fontSize:"1.3rem"
+}  
+const imgStyle ={
+    display: "block",
+    
+}
+    return (
+    <div>
+        <h2 style={{color:"red" , backgroundColor:"black"}}>React js</h2> //*içerideki süslü obje göndermeiz key value
+        <p style={pStyle}>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eveniet, illo.</p>
+        <img src="dffd" alt="sprig" />
+        <img src="" alt="" />
+        <p className="par">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Neque, illum? Deleniti nesciunt obcaecati porro nam repudiandae alias facere eaque inventore.</p>
+        <p className="par">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Tempore reiciendis dolorem eveniet reprehenderit labore repellendus repudiandae cum quia, quibusdam exercitationem iure, voluptates recusandae tenetur in sint et veniam, nostrum pariatur.</p>
+    </div>
+  )
+}
+
+export default Content
---
+++ 02-react-style/src/Footer.jsx
@@ -0,0 +1,12 @@
+
+
+const Footer = () => {
+  return (
+    <footer>
+     <p>Copyrigth by Clarusway {new Date().getFullYear()}</p>   
+    </footer>
+    
+  )
+}
+
+export default Footer
