9b0d0f92151fc7175b1b2bc1dc68b7eb82905565
nurantezer <nurantezer68@gmail.com>
2023-02-15 13:04:31 +0300

    update

--- 03-props/src/App.js
+++ 03-props/src/App.js
@@ -1,8 +1,12 @@
-
+import Person from "./components/Person";
+import { data } from "./data";
 
 function App() {
   return (
     <div>
+      {data.map(({name, img, tel})=>{
+        <Person name={name} img={img} tel={tel}/>  
+      })}
       
     </div>
   );
---
+++ 03-props/src/components/Msg.jsx
@@ -0,0 +1,10 @@
+
+const Msg = ({name}) => {
+  return (
+    <div>Msg
+        <h5>Hi, My Name is {name}</h5>
+    </div>
+  )
+}
+
+export default Msg
