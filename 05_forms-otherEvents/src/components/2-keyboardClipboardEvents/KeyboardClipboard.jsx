615c4176bdd3d2593ae546b30841bb71f5cecabf
nurantezer <nurantezer68@gmail.com>
2023-02-20 22:28:33 +0300

    update

--- 05_forms-otherEvents/src/components/2-keyboardClipboardEvents/KeyboardClipboard.jsx
+++ 05_forms-otherEvents/src/components/2-keyboardClipboardEvents/KeyboardClipboard.jsx
@@ -2,14 +2,21 @@ import { useState } from "react"
 
 const KeyboardClipboard = () => {
   const [inputData, setInputData] = useState("")
- 
+ const handleKeyDown = () =>{
+  if(e.keyCode >= 48 && e.keyCode <= 57){
+    alert("please dont enter a number")
+    e.preventDefault()
+  }
+ }
   return (
     <div className="container text-center">
       <h2 className="display-5 text-danger">Keyboard-Clipboard Event</h2>
       <input type="text" className="form-control" onChange={(e) =>{
         e.target.value = e.target.value.toUpperCase()
         setInputData(e.target.value)}
-      }  />
+      }  
+      onKeyDown = {handleKeyDown}
+      />
       <p className="text-start mt-4">{inputData.toLowerCase()}</p>
       <textarea className="form-control" name="textarea" id="textarea" cols="30" rows="10"></textarea>
     </div>
