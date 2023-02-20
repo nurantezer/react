6c9c91beb146f105a0bf3df82034813ba2d297ae
nurantezer <nurantezer68@gmail.com>
2023-02-20 20:31:11 +0300

    form

--- 05_forms-otherEvents/src/components/1-forms/Form.jsx
+++ 05_forms-otherEvents/src/components/1-forms/Form.jsx
@@ -1,5 +1,38 @@
 const Form = () => {
-  return <h1>FORMS</h1>
+  return (
+  
+     <form>
+      <h2 className="display-6 text-danger">FORMS</h2>
+
+      <div className="mb-3">
+        <label htmlFor="username" className="form-label">
+          Username
+        </label>
+        <input type="email" className="form-control" id="username" />
+      </div>
+
+      <div className="mb-3">
+        <label htmlFor="exampleInputEmail1" className="form-label">
+          Email address
+        </label>
+        <input type="email" className="form-control" id="exampleInputEmail1" />
+      </div>
+      <div className="mb-3">
+        <label htmlFor="exampleInputPassword1" className="form-label">
+          Password
+        </label>
+        <input
+          type="password"
+          className="form-control"
+          id="exampleInputPassword1"
+        />
+      </div>
+
+      <button type="submit" className="btn btn-primary">
+        Submit
+      </button>
+    </form>
+  );
 }
 
 export default Form
