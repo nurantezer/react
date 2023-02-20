2feb4fcfbbd293885db68d4d83901242491d9254
nurantezer <nurantezer68@gmail.com>
2023-02-20 12:55:12 +0300

    classcomponents

--- 04_events-useStateHook/src/App.js
+++ 04_events-useStateHook/src/App.js
@@ -1,9 +1,11 @@
 import Event from "./components/1-events/Event"
+import Counter from "./components/Counter"
 
 function App() {
   return (
     <div>
       <Event/>
+      <Counter/>
     </div>
   )
 }
