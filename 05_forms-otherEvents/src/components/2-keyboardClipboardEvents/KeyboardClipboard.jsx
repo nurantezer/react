9445b4da2c610634248e2e4bbf0c573d3c94fef4
nurantezer <nurantezer68@gmail.com>
2023-02-20 22:03:11 +0300

    go

--- 05_forms-otherEvents/src/components/2-keyboardClipboardEvents/KeyboardClipboard.jsx
+++ 05_forms-otherEvents/src/components/2-keyboardClipboardEvents/KeyboardClipboard.jsx
@@ -6,8 +6,11 @@ const KeyboardClipboard = () => {
   return (
     <div className="container text-center">
       <h2 className="display-5 text-danger">Keyboard-Clipboard Event</h2>
-      <input type="text" className="form-control" onChange={(e) => setInputData(e.target.value)} />
-      <p className="text-start mt-4">{inputData}</p>
+      <input type="text" className="form-control" onChange={(e) =>{
+        e.target.value = e.target.value.toUpperCase()
+        setInputData(e.target.value)}
+      }  />
+      <p className="text-start mt-4">{inputData.toLowerCase()}</p>
       <textarea className="form-control" name="textarea" id="textarea" cols="30" rows="10"></textarea>
     </div>
   )
