87441230a272883ccb37eff03f10058269be7230
nurantezer <nurantezer68@gmail.com>
2023-02-20 12:49:30 +0300

    event

--- 04_events-useStateHook/src/components/1-events/Event.jsx
+++ 04_events-useStateHook/src/components/1-events/Event.jsx
@@ -12,11 +12,17 @@ const handleReset = (msg) =>{
   console.log(msg)
 }
 
+function degistir(){
+  text = "Hello React"
+  console.log(text)
+}
+
   return (
     <div>
       <h1>{text}</h1>
       <button onClick={handleClick}>CLİCK</button>
       <button onClick={() => handleReset("please clear the text")}>RESET</button>
+      <button onClick={degistir}>CHANGE</button>
     </div>
   )
 }
