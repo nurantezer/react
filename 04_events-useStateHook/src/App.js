abf5a2891374e8cba4648e691d0821bb6eb4149e
nurantezer <nurantezer68@gmail.com>
2023-02-20 13:11:20 +0300

    usestate

--- 04_events-useStateHook/src/App.js
+++ 04_events-useStateHook/src/App.js
@@ -1,11 +1,13 @@
 import Event from "./components/1-events/Event"
 import Counter from "./components/Counter"
+import UseStateCounter from "./components/UseStateCounter"
 
 function App() {
   return (
     <div>
       <Event/>
       <Counter/>
+      <UseStateCounter/>
     </div>
   )
 }
---
+++ 04_events-useStateHook/src/components/UseStateCounter.jsx
@@ -0,0 +1,9 @@
+import {useState} from 'react'
+
+const UseStateCounter = () => {
+  return (
+    <div>UseStateCounter</div>
+  )
+}
+
+export default UseStateCounter
