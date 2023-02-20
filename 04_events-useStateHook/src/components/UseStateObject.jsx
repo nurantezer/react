c3f957215d3b5c66943829d6c62df82f81763921
nurantezer <nurantezer68@gmail.com>
2023-02-20 13:32:49 +0300

    ınc

--- 04_events-useStateHook/src/components/UseStateObject.jsx
+++ 04_events-useStateHook/src/components/UseStateObject.jsx
@@ -4,15 +4,20 @@ const UseStateObject = () => {
  const [person, setPerson] = useState({
     name: "ahmet",
     surname: "can",
-    salary: "10000",
+    salary: 10000,
  })
 console.log(person)
-
+const handleSalaryInc = () =>{
+    setPerson({...person, salary: person.salary + 500})
+}
 
 
   return (
     <div className='container text-center mt-4'>
     <h2 className='text-danger'>Use State Object</h2>
+    <h3 className='display-4'>{person.name} {person.surname}</h3>
+    <h3 className='display-6'>Salary:{person.salary}</h3>
+    <button className='btn btn-success' onClick={handleSalaryInc}>SALARY INC</button>
 
     </div>
   )
