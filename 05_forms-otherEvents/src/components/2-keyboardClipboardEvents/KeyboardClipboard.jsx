e297708f101272fc6b5eaad17253c8f9077cd925
nurantezer <nurantezer68@gmail.com>
2023-02-20 21:59:11 +0300

    inputdata

--- 05_forms-otherEvents/src/components/2-keyboardClipboardEvents/KeyboardClipboard.jsx
+++ 05_forms-otherEvents/src/components/2-keyboardClipboardEvents/KeyboardClipboard.jsx
@@ -1,7 +1,14 @@
+import { useState } from "react"
+
 const KeyboardClipboard = () => {
+  const [inputData, setInputData] = useState("")
+ 
   return (
     <div className="container text-center">
-      <h2>Keyboard-Clipboard Event</h2>
+      <h2 className="display-5 text-danger">Keyboard-Clipboard Event</h2>
+      <input type="text" className="form-control" onChange={(e) => setInputData(e.target.value)} />
+      <p className="text-start mt-4">{inputData}</p>
+      <textarea className="form-control" name="textarea" id="textarea" cols="30" rows="10"></textarea>
     </div>
   )
 }
