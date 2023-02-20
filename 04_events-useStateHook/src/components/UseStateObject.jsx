1ed9e1b26239986b52ada6dfe8af5472ba197f2a
nurantezer <nurantezer68@gmail.com>
2023-02-20 13:28:26 +0300

    go

--- 04_events-useStateHook/src/components/UseStateObject.jsx
+++ 04_events-useStateHook/src/components/UseStateObject.jsx
@@ -1,8 +1,20 @@
 import {useState} from 'react'
 
 const UseStateObject = () => {
+ const [person, setPerson] = useState({
+    name: "ahmet",
+    surname: "can",
+    salary: "10000",
+ })
+console.log(person)
+
+
+
   return (
-    <div>UseStateObject</div>
+    <div className='container text-center mt-4'>
+    <h2 className='text-danger'>Use State Object</h2>
+
+    </div>
   )
 }
 
