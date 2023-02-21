dc9268f80a1bd3f462d62dcc89b2ee88cc3ee712
nurantezer <nurantezer68@gmail.com>
2023-02-21 08:28:28 +0300

    form start

--- 05_forms-otherEvents/src/components/1-forms/Form.jsx
+++ 05_forms-otherEvents/src/components/1-forms/Form.jsx
@@ -2,7 +2,32 @@
 
 const Form = () => {
   return (
-    
+    <form>
+        <h2 className="display-6 text-danger">FORMS</h2>
+
+        <div className="mb-3">
+            <label htmlFor="username" className="form-label">
+                Username: <span>{}</span>
+            </label>
+            <input type="text" className="form-control" id="username" required />
+        </div>
+
+        <div className="mb-3">
+            <label htmlFor="email" className="form-label">
+                Email: <span>{}</span>
+            </label>
+            <input type="email" className="form-control" id="email" required />
+        </div>
+
+        <div className="mb-3">
+            <label htmlFor="password" className="form-label">
+                Password
+            </label>
+            <input type="password" className="form-control" id="password" required />
+        </div>
+
+        <button type="submit" className="btn btn-danger">Submit</button>
+    </form>
   )
 }
 
