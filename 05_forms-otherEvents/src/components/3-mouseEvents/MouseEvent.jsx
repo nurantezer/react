d5da0f8ae5b43833877da4c10e0c6ec07b8ea5d6
nurantezer <nurantezer68@gmail.com>
2023-02-21 12:13:51 +0300

    end

--- 05_forms-otherEvents/src/components/3-mouseEvents/MouseEvent.jsx
+++ 05_forms-otherEvents/src/components/3-mouseEvents/MouseEvent.jsx
@@ -3,7 +3,24 @@ import { useState } from "react";
 
 const MouseEvent = () => {
 const [visible, setVisible] = useState(false)
+const [toggle, setToggle] = useState(false)
 
+const [coordX, setCoordX] = useState()
+const [coordY, setCoordY] = useState()
+
+const handleDoubleClick = (e) => {
+  setToggle(!toggle);
+  // console.log(toggle) //? State'in guncellenmis degeri burada gorulemeyebilir.Cunku react'da state'ler toplu bir sekilde ve async olarak guncellenir.
+
+  toggle
+    ? (e.target.className = "bg-success text-light w-50 p-4 my-4")
+    : (e.target.className = "bg-danger text-light w-50 p-4 my-4")
+}
+
+const handleMouseMove = (e) => {
+    setCoordX(e.pageX)
+    setCoordY(e.pageY)
+}
 
   return (
     <div className="container text-center d-flex flex-column align-items-center mt-4">
@@ -17,16 +34,27 @@ const [visible, setVisible] = useState(false)
       >
         todo item 1
       </div>
-        {visible &&<div>Mouse Events</div>}
-      <div className="bg-success text-light w-50 p-4 mt-4" 
-      id="todo-2">
+      {visible && <div>Mouse Events</div>}
+      <div
+        className="bg-success text-light w-50 p-4 mt-4"
+        id="todo-2"
+        onDoubleClick={handleDoubleClick}
+      >
         todo item 2
-        </div>
+      </div>
 
-        <div className="bg-success text-light w-50 p-4 mt-4" 
-        id="todo-3">
-            todo item 3
-            </div>
+      <div
+        className="bg-success text-light w-50 p-4 mt-4"
+        id="todo-3"
+        onMouseMove={handleMouseMove}
+      >
+         todo item 3
+      </div>
+
+      <p>X and Y</p>
+      <p className="text-danger fw-bold">
+        {coordX} {coordY}
+      </p>
     </div>
   );
 }
