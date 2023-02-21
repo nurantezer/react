2713bf19ad26efe89a6ab596a640aebdc54aeb0c
nurantezer <nurantezer68@gmail.com>
2023-02-21 11:59:02 +0300

    visible

--- 05_forms-otherEvents/src/components/3-mouseEvents/MouseEvent.jsx
+++ 05_forms-otherEvents/src/components/3-mouseEvents/MouseEvent.jsx
@@ -1,9 +1,34 @@
-import React from 'react'
+import { useState } from "react";
+
 
 const MouseEvent = () => {
+const [visible, setVisible] = useState(false)
+
+
   return (
-    <div>MouseEvent</div>
-  )
+    <div className="container text-center d-flex flex-column align-items-center mt-4">
+      <h2 className="text-danger">MOUSE EVENTS</h2>
+
+      <div
+        id="todo-1"
+        className="bg-success text-light w-50 p-4"
+        onMouseOver={() => setVisible(true)}
+        onMouseOut={() => setVisible(false)}
+      >
+        todo item 1
+      </div>
+        {visible &&<div>Mouse Events</div>}
+      <div className="bg-success text-light w-50 p-4 mt-4" 
+      id="todo-2">
+        todo item 2
+        </div>
+
+        <div className="bg-success text-light w-50 p-4 mt-4" 
+        id="todo-3">
+            todo item 3
+            </div>
+    </div>
+  );
 }
 
 export default MouseEvent
