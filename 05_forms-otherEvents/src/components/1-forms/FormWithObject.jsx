0d307e2e0955e4a444b37f0c9425d95d75a96a35
nurantezer <nurantezer68@gmail.com>
2023-02-21 09:08:02 +0300

    username

--- 05_forms-otherEvents/src/components/1-forms/FormWithObject.jsx
+++ 05_forms-otherEvents/src/components/1-forms/FormWithObject.jsx
@@ -1,8 +1,36 @@
-import React from 'react'
+import { useState } from "react"
+
 
 const FormWithObject = () => {
+    const [formData, setFormData] = useState({
+        username:"",
+        email:"",
+        password:"",
+        address:"",
+    })
+
+    const {username, email, password, address} = formData
+
+
+
+
   return (
-    <div>FormWithObject</div>
+    <form>
+        <h2 className="display-6 text-danger">FORM WİTH OBJECT</h2>
+
+        <div className="mb-3">
+            <label htmlFor="username" className='form-label'>
+                Username: <span>{username}</span>
+            </label>
+            <input type="text"
+            className='form-control'
+            id='usermane'
+            value={username || ""} //* username yoksa inputun valusuna null ata
+            //! controlled- uncontrolled error (eğer bir inputun valuesu undifened olursa böyle bir hata gelir)
+            />
+        </div>
+        
+    </form>
   )
 }
 
