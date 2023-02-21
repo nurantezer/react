a6028ea0351eb59e56c2f5d368750e4ded04659c
nurantezer <nurantezer68@gmail.com>
2023-02-21 10:44:00 +0300

    go

--- 05_forms-otherEvents/src/components/2-keyboardClipboardEvents/KeyboardClipboard.jsx
+++ 05_forms-otherEvents/src/components/2-keyboardClipboardEvents/KeyboardClipboard.jsx
@@ -1,8 +1,23 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const KeyboardClipboard = () => {
+    const [inputData, setInputData] = useState("")
+
+
+
+
   return (
-    <div>KeyboardClipboard</div>
+    <div className='container text-center'>
+        <h2 className="display-5 text-danger">Keyboard-Clipboard Event</h2>
+
+        <input type="text"
+        className='form-control'
+        onChange={(e) => {
+            e.target.value = e.target.value.toLocaleUpperCase()
+            setInputData(e.target.value)
+        }} 
+         />
+    </div>
   )
 }
 
