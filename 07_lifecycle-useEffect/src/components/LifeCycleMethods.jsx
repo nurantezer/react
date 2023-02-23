455d4714682ed37c83ab73a812a7464dba703f6e
nurantezer <nurantezer68@gmail.com>
2023-02-23 15:07:20 +0300

    go

--- 07_lifecycle-useEffect/src/components/LifeCycleMethods.jsx
+++ 07_lifecycle-useEffect/src/components/LifeCycleMethods.jsx
@@ -2,7 +2,9 @@
 import React from 'react'
 
 export class LifeCycleMethods extends React.Component {
+  //!1-) Bir componentin olusturulmasında cagrılır
 constructor(props) {
+  console.log("1- Constructor running")
   super(props)
 
   this.state = {
@@ -16,8 +18,16 @@ handleInc = () => {
   })
 }
 
+//!3-) Bir componet DOM ağacına eklendiğinde calıstırır.
+//!(ilk render sonrası)
+//!Her yasam dongusu için bir kere calisir
+componentDidMount() {
+  console.log("3-componenddidmount")
+}
 
   render() {
+    //! 2-) Her bir state ya da prop degistiginde (render) cagrilir
+    console.log("2-Rendered")
     return (
       <div className='container text-center'>
         <h1 className='text-danger'>LIFECYLE METHODS</h1>
