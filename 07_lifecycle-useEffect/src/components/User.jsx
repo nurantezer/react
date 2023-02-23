f828c3f4db6bb262547f2f6c4aa8331b05563583
nurantezer <nurantezer68@gmail.com>
2023-02-23 16:03:20 +0300

    fetch end

---
+++ 07_lifecycle-useEffect/src/components/User.jsx
@@ -0,0 +1,39 @@
+import { useEffect, useState } from "react"
+
+
+const User = () => {
+    const [user, setUser] = useState()
+
+    const getUser = () => {
+      fetch("https://randomuser.me/api/")
+        .then((res) => res.json())
+        .then((data) => setUser(data.results[0]))
+        .catch((err) => console.log(err));
+    };
+
+    useEffect(() => {
+      console.log("Mounting")
+      getUser()
+     
+    }, [])
+    
+  return (
+    <div>
+      <h1>
+        {/* Optional Chaingn */}
+        {user?.name?.first} {user?.name?.last}
+      </h1>
+      <img className="rounded-circle" src={user?.picture?.large} alt="" />
+      <h4>{user?.email}</h4>
+      <h5>{user?.phone}</h5>
+      {/* <p>{user?.dob?.date}</p> */}
+      <p>{new Date(user?.dob?.date).toLocaleDateString("tr-TR")}</p>
+
+      <button className="btn btn-warning" onClick={getUser}>
+        Get User
+      </button>
+    </div>
+  );
+}
+
+export default User
