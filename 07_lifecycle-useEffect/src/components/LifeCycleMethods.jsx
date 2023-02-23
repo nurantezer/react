d55570cc2437d883425fa0c901fedadb6c320da6
nurantezer <nurantezer68@gmail.com>
2023-02-23 15:02:05 +0300

    go

--- 07_lifecycle-useEffect/src/components/LifeCycleMethods.jsx
+++ 07_lifecycle-useEffect/src/components/LifeCycleMethods.jsx
@@ -1,10 +1,30 @@
 
-import React, { Component } from 'react'
+import React from 'react'
 
-export class LifeCycleMethods extends Component {
+export class LifeCycleMethods extends React.Component {
+constructor(props) {
+  super(props)
+
+  this.state = {
+    count: 0,
+  }
+}
+
+handleInc = () => {
+  this.setState({
+    count: this.state.count + 1,
+  })
+}
+
+
   render() {
     return (
-      <div>LifeCycleMethods</div>
+      <div className='container text-center'>
+        <h1 className='text-danger'>LIFECYLE METHODS</h1>
+        <h3>COUNT={this.state.count}</h3>
+        <button className='btn btn-info' onClick={this.handleInc}>INC</button>
+
+      </div>
     )
   }
 }
