29f728151cb1431e8e3a4df62f9859f2954e382c
nurantezer <nurantezer68@gmail.com>
2023-02-26 10:06:51 +0300

    nba

--- 06_nba-legends/src/App.js
+++ 06_nba-legends/src/App.js
@@ -1,9 +1,10 @@
+import Container from "react-bootstrap/Container";
 import Header from "./components/Header";
-import Container from "react-bootsrap/Container";
 function App() {
   return (
     <Container className="text-center mt-4">
-      <Header/>
+      <Header />
+    
     </Container>
   );
 }
--- 06_nba-legends/src/components/Header.jsx
+++ 06_nba-legends/src/components/Header.jsx
@@ -1,13 +1,8 @@
-import nbaLogo from "../assets/nba-logo.png"
-import Container from "react-bootsrap/Container"
-import Image from "react-boostrap/Image"
+
+
 const Header = () => {
   return (
-    <Container>
-        <Image src={nbaLogo} width= "200px"></Image>
-        <h1 className="my-2 font-monospace display-4 fw-bold">NBA Legends</h1>//*my altan üsten
-        //*margin verme
-    </Container>
+    <div>Header</div>
   )
 }
 
