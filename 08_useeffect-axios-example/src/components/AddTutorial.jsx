20dea39e26a676ca9b4168f8632d47fccaa12060
nurantezer <nurantezer68@gmail.com>
2023-02-26 21:04:29 +0300

    end

--- 08_useeffect-axios-example/src/components/AddTutorial.jsx
+++ 08_useeffect-axios-example/src/components/AddTutorial.jsx
@@ -1,60 +1,9 @@
-import axios from "axios"
-import { useState } from "react"
+import React from "react";
 
-
-const AddTutorial = ({getTutorials}) => {
-  const [title, setTitle] = useState("")
-  const [description, setDescription] = useState("")
-  const handleSubmit = (e) => {
-    e.preventDefault()
-    const newTutorial = {title, description}
-    postTutorial(newTutorial)         //!yeni tutorial ekledim
-    setDescription("")
-    setTitle("")
-  }
-  
-  const postTutorial = async (newTutorial) =>{
-  const BASE_URL = "https://tutorial-api.fullstack.clarusway.com/tutorials/";
-  try {
-    await axios.post(BASE_URL,newTutorial)
-  } catch (error) {
-    console.log(error);
-  }
-  getTutorials()
-  }
-
+const AddTutorial = () => {
   return (
-    <div className='container text-center mt-4'>
-      <h1 className='display-6 text-danger'>Add Your Tutorial</h1>
-      <form onSubmit={handleSubmit}>
-        <div className="mb-3">
-          <label htmlFor="title" className="form-label">Title</label>
-          <input type="text"
-          className="form-control"
-          id="title"
-          value={title}
-          onChange={(e) => setTitle(e.target.value)}
-          placeholder="Enter your title"
-          required
-          />
-        </div>
-        <div className="mb-3">
-          <label htmlFor="desc" className="form-label">Description</label>
-          <input type="text"
-          className="form-control"
-          id="desc"
-          value={description}
-          onChange={(e) => setDescription(e.target.value)}
-          placeholder="Enter your Description"
-          required
-          />
-        </div>
-        <button type="submit" className="btn btn-danger mb-4">Submit</button>
-
-      </form>
-
-    </div>
+  <div>AddTutorial</div>
   )
-}
+};
 
-export default AddTutorial
+export default AddTutorial;
--- 08_useeffect-axios-example/src/pages/Home.jsx
+++ 08_useeffect-axios-example/src/pages/Home.jsx
@@ -1,34 +1,9 @@
-import axios from "axios"
-import { useEffect, useState } from 'react'
-import AddTutorial from '../components/AddTutorial'
-import TutorialList from '../components/TutorialList'
+import React from "react";
 
 const Home = () => {
-  const [tutorials, setTutorials] = useState([]) //!json formatında gelecek veriler
-  
-  const BASE_URL ="https://tutorial-api.fullstack.clarusway.com/tutorials/"
-  
-  const getTutorials = async () =>{
-    try {
-      const {data} = await axios(BASE_URL)  //!Data havada desc edildi
-      setTutorials(data)
-    } catch (error) {
-      console.log(error)
-    }
-  }
-  console.log(tutorials);
- useEffect(() => {
-   getTutorials()
- }, [])
- 
-
-  
   return (
-    <>
-    <AddTutorial getTutorials={getTutorials}/>
-    <TutorialList tutorials={tutorials} getTutorials={getTutorials}/>
-    </>
+  <div>Home</div>
   )
-}
+};
 
-export default Home
+export default Home;
