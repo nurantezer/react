de2b9921718ba27cb9dbf91db6219ef99799015f
nurantezer <nurantezer68@gmail.com>
2023-02-26 10:49:44 +0300

    container end

--- 06_nba-legends/src/components/CardContainer.jsx
+++ 06_nba-legends/src/components/CardContainer.jsx
@@ -9,21 +9,25 @@ const CardContainer = () => {
     const [search, setSearch] = useState("")
   return (
     <>
-    <Form.Control
-    placeholder='Search Player...'
-    type='searc'
-    className='w-50 m-auto'
-    onChange={(e) => setSearch(e.target.value)}
-    />
+      <Form.Control
+        placeholder="Search Player..."
+        type="searc"
+        className="w-50 m-auto"
+        onChange={(e) => setSearch(e.target.value)}
+      />
       <Container className="card-container rounded-4 my-4 p-3">
         <Row className="g-3 justify-content-center">
-          {data.map((player, index) => {
-            return (
-              <Col md={6} lg={4} xl={3} key={index}>
-                <PlayerCard {...player} />
-              </Col>
-            );
-          })}
+          {data
+            .filter((player) =>
+              player.name.toLowerCase().includes(search.trim().toLowerCase())
+            )
+            .map((player, i) => {
+              return (
+                <Col md={6} lg={4} xl={3} key={i}>
+                  <PlayerCard {...player} />
+                </Col>
+              );
+            })}
         </Row>
       </Container>
     </>
