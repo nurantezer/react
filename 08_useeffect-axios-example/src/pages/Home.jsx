de0cd6059f00837ab145c351484ae8c0059e24e5
nurantezer <nurantezer68@gmail.com>
2023-02-27 08:31:04 +0300

    home end

--- 08_useeffect-axios-example/src/pages/Home.jsx
+++ 08_useeffect-axios-example/src/pages/Home.jsx
@@ -1,8 +1,31 @@
-import React from 'react'
+import axios from "axios"
+import { useEffect, useState } from "react"
+import Addtutorial from "../components/AddTutorial"
+import TutorialList from "../components/TutorialList"
 
 const Home = () => {
+  const [tutorials, setTutorials] = useState([])
+
+  const BASE_URL = "https://tutorial-api.fullstack.clarusway.com/tutorials/";
+
+  const getTutorials = async () => {
+  try {
+    const { data } = axios(BASE_URL)
+    setTutorials(data)
+  } catch (error) {
+    console.log(error);
+  }
+}
+  useEffect(() => {
+    getTutorials()
+  }, [])
+  
+
   return (
-    <div>Home</div>
+    <>
+      <Addtutorial getTutorials={getTutorials} />
+      <TutorialList tutorials={tutorials} getTutorials={getTutorials} />
+    </>
   )
 }
 
