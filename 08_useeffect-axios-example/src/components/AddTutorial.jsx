d625a4d08f43797a26346a4ad207a5b2f25c53f5
nurantezer <nurantezer68@gmail.com>
2023-02-27 09:32:22 +0300

    end

--- 08_useeffect-axios-example/src/components/AddTutorial.jsx
+++ 08_useeffect-axios-example/src/components/AddTutorial.jsx
@@ -1,24 +1,39 @@
 import { useState } from "react";
+import axios from "axios";
 
-
-const AddTutorial = () => {
-  const [title, setTitle] = useState("")
-  const [description, setDescription] = useState("")
+const AddTutorial = ({ getTutorials }) => {
+  const [title, setTitle] = useState("");
+  const [description, setDescription] = useState("");
 
   const handleSubmit = (e) => {
-    e.preventDefault()
-    setTitle("")
-    description("")
-  }
+    e.preventDefault();
+    const newTutorial = { title, description };
+    postTutorial(newTutorial);
+    setDescription("");
+    setTitle("");
+  };
 
+  const postTutorial = async (newTutorial) => {
+    const BASE_URL = "https://tutorial-api.fullstack.clarusway.com/tutorials/";
+    try {
+      await axios.post(BASE_URL, newTutorial);
+    } catch (error) {
+      console.log(error);
+    }
+    getTutorials();
+  };
 
+  console.log(title, description);
   return (
     <div className="container text-center mt-4">
       <h1 className="display-6 text-danger">Add Your Tutorial</h1>
       <form onSubmit={handleSubmit}>
         <div className="mb-3">
-          <label htmlFor="title" className="form-label">Title</label>
-          <input type="text"
+          <label htmlFor="title" className="form-label">
+            Title
+          </label>
+          <input
+            type="text"
             className="form-control"
             id="title"
             placeholder="Enter your title"
@@ -28,19 +43,25 @@ const AddTutorial = () => {
           />
         </div>
         <div className="mb-3">
-          <label htmlFor="desc" className="form-label">Description</label>
-          <input type="text"
+          <label htmlFor="desc" className="form-label">
+            Description
+          </label>
+          <input
+            type="text"
             className="form-control"
+            id="desc"
             placeholder="Enter your Description"
             value={description}
             onChange={(e) => setDescription(e.target.value)}
+            required
           />
         </div>
-        <button type="submit" className="btn btn-danger mb-4">Submit</button>
+        <button type="submit" className="btn btn-danger mb-4">
+          Submit
+        </button>
       </form>
-      
-  </div>
-  )
+    </div>
+  );
 };
 
 export default AddTutorial;
--- 08_useeffect-axios-example/src/pages/Home.jsx
+++ 08_useeffect-axios-example/src/pages/Home.jsx
@@ -1,32 +1,33 @@
-import axios from "axios"
-import { useEffect, useState } from "react"
-import Addtutorial from "../components/AddTutorial"
-import TutorialList from "../components/TutorialList"
+import axios from "axios";
+import { useEffect, useState } from "react";
+import AddTutorial from "../components/AddTutorial";
+import TutorialList from "../components/TutorialList";
 
 const Home = () => {
-  const [tutorials, setTutorials] = useState([])
+  const [tutorials, setTutorials] = useState([]);
 
   const BASE_URL = "https://tutorial-api.fullstack.clarusway.com/tutorials/";
 
   const getTutorials = async () => {
-  try {
-    const { data } = axios(BASE_URL)
-    setTutorials(data)
-  } catch (error) {
-    console.log(error);
-  }
-}
+    try {
+      const { data } = await axios(BASE_URL);
+      setTutorials(data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  console.log(tutorials);
+  //? componentDidMount
   useEffect(() => {
-    getTutorials()
-  }, [])
-  
+    getTutorials();
+  }, []);
 
   return (
     <>
-      <Addtutorial getTutorials={getTutorials} />
+      <AddTutorial getTutorials={getTutorials} />
       <TutorialList tutorials={tutorials} getTutorials={getTutorials} />
     </>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
