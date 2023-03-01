42a5ef0d8f33a388608a554457deea6b36f23497
nurantezer <nurantezer68@gmail.com>
2023-03-01 18:25:59 +0300

    nav

--- 10_router-intro/src/components/Nav.jsx
+++ 10_router-intro/src/components/Nav.jsx
@@ -1,8 +1,32 @@
-import React from 'react'
+import {Link, NavLink} from "react-router-dom"
 
 const Nav = () => {
   return (
-    <div>Nav</div>
+    <ul>
+      <li>
+        <Link to="/">Home</Link>
+      </li>
+      <li>
+        <NavLink
+          to="/instructors"
+          style={({ isActive }) => ({
+            color: isActive && "red",
+          })}
+        >
+          Instructors
+        </NavLink>
+      </li>
+      <li>
+        <NavLink
+          to="/contact"
+          style={({ isActive }) => ({
+            color: isActive && "red",
+          })}
+        >
+          Contact
+        </NavLink>
+      </li>
+    </ul>
   )
 }
 
