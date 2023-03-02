bbeaa7877ef817a0556f68265559181ab04fd883
nurantezer <nurantezer68@gmail.com>
2023-03-02 13:12:39 +0300

    about

--- 11_router/src/App.js
+++ 11_router/src/App.js
@@ -1,12 +1,13 @@
 import Nav from "./components/Nav"
 import { Routes, Route } from "react-router-dom"
 import Footer from "./components/Footer"
+import Home from "./pages/Home"
 function App() {
   return (
     <div >
       <Nav />
       <Routes>
-      <Route/>
+        <Route path="/" element={<Home/>} />
       </Routes>
       <Footer/>
     </div>
