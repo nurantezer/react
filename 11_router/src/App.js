c64f0fbc3f83024b7c131522f40ec07588986218
nurantezer <nurantezer68@gmail.com>
2023-03-02 13:56:51 +0300

    persondetail

--- 11_router/src/App.js
+++ 11_router/src/App.js
@@ -3,6 +3,7 @@ import { Routes, Route } from "react-router-dom"
 import Footer from "./components/Footer"
 import Home from "./pages/Home"
 import People from "./pages/People"
+import PersonDetail from "./pages/PersonDetail"
 function App() {
   return (
     <div >
@@ -10,6 +11,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/people" element={<People />} />
+        <Route path="/people/:id" element={<PersonDetail />} />
         
       </Routes>
       <Footer/>
--- 11_router/src/pages/People.jsx
+++ 11_router/src/pages/People.jsx
@@ -1,8 +1,42 @@
-import React from 'react'
+import { useEffect, useState } from "react"
+import{useNavigate} from "react-router-dom"
 
 const People = () => {
+  const [people, setPeople] = useState([])
+  const navigate = useNavigate()
+  const getPeople = () => {
+    fetch("https://reqres.in/api/users")
+      .then((res) => res.json())
+      .then((data) => setPeople(data.data))
+      .catch((err) => console.log(err))
+  }
+  useEffect(() => {
+  getPeople()
+  }, [])
+  
+
   return (
-    <div>People</div>
+    <div className="container text-center mt-4">
+      <h1>PEOPLE LIST</h1>
+      <div className="row justfiy-content-center g-3">
+        {people?.map((p) => {
+          const { id, first_name, last_name, avatar } = p
+          return (
+            <div key={id}
+              className="col-sm-12 col-md-6 col-lg-4"
+              type="button"
+              onClick={() => navigate(`${id}` , {state: p})}
+            >
+              <img className="rounded" src={avatar} alt="img" />
+              <h6>
+                {first_name} {last_name}
+              </h6>
+            </div>
+          )
+      })}
+      </div>
+
+    </div>
   )
 }
 
