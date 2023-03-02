99eba619a6717fdb277ddf0c627ea83790e8e6c0
nurantezer <nurantezer68@gmail.com>
2023-03-02 13:26:34 +0300

    people

--- 11_router/src/App.js
+++ 11_router/src/App.js
@@ -2,12 +2,15 @@ import Nav from "./components/Nav"
 import { Routes, Route } from "react-router-dom"
 import Footer from "./components/Footer"
 import Home from "./pages/Home"
+import People from "./pages/People"
 function App() {
   return (
     <div >
       <Nav />
       <Routes>
-        <Route path="/" element={<Home/>} />
+        <Route path="/" element={<Home />} />
+        <Route path="/people" element={<People />} />
+        
       </Routes>
       <Footer/>
     </div>
