2a6f8296588475cdc095e76d0fc560ece0924231
nurantezer <nurantezer68@gmail.com>
2023-03-02 14:47:46 +0300

    notfound

--- 11_router/src/App.js
+++ 11_router/src/App.js
@@ -5,6 +5,8 @@ import Home from "./pages/Home"
 import People from "./pages/People"
 import PersonDetail from "./pages/PersonDetail"
 import Contact from "./pages/Contact"
+import Paths from "./pages/Paths"
+import NotFound from "./pages/NotFound"
 function App() {
   return (
     <div >
@@ -14,7 +16,8 @@ function App() {
         <Route path="/people" element={<People />} />
         <Route path="/people/:id" element={<PersonDetail />} />
         <Route path="/contact" element={<Contact/>} />
-        
+        <Route path="/paths" element={<Paths/>} />
+        <Route path="*" element={<NotFound/>} />
       </Routes>
       <Footer/>
     </div>
