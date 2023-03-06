f650c53547b3627c612dd8781140b0584490b7b6
nurantezer <nurantezer68@gmail.com>
2023-03-06 11:57:45 +0300

    home

--- 09-appoinment-project/src/pages/Home.jsx
+++ 09-appoinment-project/src/pages/Home.jsx
@@ -1,8 +1,25 @@
-import React from 'react'
+import { useState } from "react"
+import { appointmentData, doctorData } from "../helper/data"
+import Doctors from "../components/Doctors"
+import AppointmentList from "../components/AppointmentList"
 
 const Home = () => {
+    const [doctors, setDoctors] = useState(doctorData)
+    const [appointments, setAppointments] = useState(appointmentData)
   return (
-    <div>Home</div>
+      <main className="text-center mt-2">
+          <h1 className="display-5 text-danger">CLARUS HOSPİTRAL</h1>
+          <Doctors
+              doctors={doctors}
+              appointments={appointments}
+              setAppointments={setAppointments}
+          />
+          <AppointmentList
+              appointments={appointments}
+              setAppointments={setAppointments}
+          />
+          
+    </main>
   )
 }
 
