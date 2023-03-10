70dfef10ce7b59f14d35a73f101c95113c910be8
nurantezer <nurantezer68@gmail.com>
2023-03-10 13:03:17 +0300

    container

--- 12_styled/src/App.js
+++ 12_styled/src/App.js
@@ -1,15 +1,20 @@
 import { Button, FS13Button } from "./components/Buton.styled";
 import HeaderText from "./components/HeaderText";
+import Sarmal from "./components/Sarmal";
 
 const App = () => {
   return (
     <>
-      <HeaderText renk="hotpink">Styled Components</HeaderText>
-      <HeaderText renk="#734586" bgRenk="#eee">Styled Components</HeaderText>
-      <Button>Send</Button>
-      <Button primary>Send</Button>
-      <FS13Button react>CALIS</FS13Button>
-      <FS13Button>DINLEN</FS13Button>
+      <Sarmal>
+        <HeaderText renk="hotpink">Styled Components</HeaderText>
+        <HeaderText renk="#734586" bgRenk="#eee">
+          Styled Components
+        </HeaderText>
+        <Button>Send</Button>
+        <Button primary>Send</Button>
+        <FS13Button react>CALIS</FS13Button>
+        <FS13Button>DINLEN</FS13Button>
+      </Sarmal>
     </>
   );
 };
