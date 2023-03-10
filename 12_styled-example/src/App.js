c73b50e380cb59de3d454f8a3e43afcb385f0144
nurantezer <nurantezer68@gmail.com>
2023-03-10 14:00:28 +0300

    header

--- 12_styled-example/src/App.js
+++ 12_styled-example/src/App.js
@@ -1,6 +1,8 @@
 import { ThemeProvider } from "styled-components";
+import Header from "./components/Header";
 import Nav from "./components/Nav";
-
+import Container from "./components/styled/Conteiner";
+import { GlobalStyles } from "./components/styled/GlobalStyles";
 const styles = {
   colors: {
     primary: "#eee",
@@ -14,7 +16,11 @@ const styles = {
 const App = () => {
   return (
     <ThemeProvider theme={styles}>
+      <GlobalStyles/>
       <Nav />
+      <Container>
+        <Header/>
+      </Container>
     </ThemeProvider>
   );
 };
---
+++ 12_styled-example/src/components/styled/Flex.jsx
@@ -0,0 +1,20 @@
+import styled from "styled-components";
+
+const Flex = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  padding: 1.5rem;
+  gap: 1rem;
+
+  @media (max-width: ${({ theme }) => theme.breakpoints.md}) {
+    flex-direction: column;
+    text-align: center;
+  }
+
+  & div,
+  & ul {
+    flex: 1;
+  }
+`;
+export default Flex
