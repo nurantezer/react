3920b3ca4d235576855220f10ffdad5f84387f58
nurantezer <nurantezer68@gmail.com>
2023-03-15 14:20:46 +0300

    login

--- 13_context-example/src/App.js
+++ 13_context-example/src/App.js
@@ -3,6 +3,7 @@ import Navs from "./components/Navs";
 import LoginProvider from "./context/LoginProvider";
 import Home from "./pages/Home";
 import About from "./pages/About";
+import Login from "./pages/Login";
 function App() {
   return (
     <LoginProvider>
@@ -10,7 +11,8 @@ function App() {
         <Navs />
         <Routes>
           <Route index element={<Home />} />
-          <Route path="about" element={<About/>} />
+          <Route path="about" element={<About />} />
+          <Route path="login" element={<Login/>} />
         </Routes>
       </BrowserRouter>
     </LoginProvider>
--- 13_context-example/src/pages/Login.jsx
+++ 13_context-example/src/pages/Login.jsx
@@ -1,9 +1,51 @@
-import React from 'react'
+import { useContext, useState } from "react";
+import Container from "react-bootstrap/Container";
+import Button from "react-bootstrap/Button";
+import Form from "react-bootstrap/Form";
+import { useLoginContext } from "../context/LoginProvider";
+import { useNavigate } from "react-router-dom";
 
 const Login = () => {
+    const { user, setUser } = useLoginContext()
+    
+    const navigate = useNavigate()
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        navigate(-1)
+    }
   return (
-    <div>Login</div>
-  )
-}
+    <Container>
+      <h1 className="text-center mt-4">LOGIN PAGE</h1>
+      <Form onSubmit={(e) => handleSubmit(e)}>
+        <Form.Group className="mb-3" controlId="username">
+          <Form.Label>Email</Form.Label>
+          <Form.Control
+            type="email"
+            placeholder="Enter your email"
+            name="email"
+            value={user?.email}
+            onChange={(e) => setUser({ ...user, email: e.target.value })}
+          />
+        </Form.Group>
 
-export default Login
+        <Form.Group className="mb-3" controlId="password">
+          <Form.Label>Password</Form.Label>
+          <Form.Control
+            type="password"
+            placeholder="Enter your password"
+            name="password"
+            value={user?.password}
+            onChange={(e) => setUser({ ...user, password: e.target.value })}
+          />
+        </Form.Group>
+        <Container className="text-center">
+          <Button variant="danger" type="submit">
+            Submit
+          </Button>
+        </Container>
+      </Form>
+    </Container>
+  );
+};
+
+export default Login;
