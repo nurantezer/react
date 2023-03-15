d4f38b4ee460a2e51610eefc3039547a4fdc154f
nurantezer <nurantezer68@gmail.com>
2023-03-15 14:15:17 +0300

    about

--- 13_context-example/src/App.js
+++ 13_context-example/src/App.js
@@ -2,7 +2,7 @@ import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import Navs from "./components/Navs";
 import LoginProvider from "./context/LoginProvider";
 import Home from "./pages/Home";
-
+import About from "./pages/About";
 function App() {
   return (
     <LoginProvider>
@@ -10,7 +10,7 @@ function App() {
         <Navs />
         <Routes>
           <Route index element={<Home />} />
-          
+          <Route path="about" element={<About/>} />
         </Routes>
       </BrowserRouter>
     </LoginProvider>
