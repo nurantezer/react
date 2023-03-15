5336714c1df30e562a46c26f5879d86b1448ea76
nurantezer <nurantezer68@gmail.com>
2023-03-15 14:34:02 +0300

    people

--- 13_context-example/src/App.js
+++ 13_context-example/src/App.js
@@ -4,6 +4,9 @@ import LoginProvider from "./context/LoginProvider";
 import Home from "./pages/Home";
 import About from "./pages/About";
 import Login from "./pages/Login";
+import People from "./pages/People";
+import PrivateRouter from "./pages/PrivateRouter";
+import PersonDetail from "./pages/PersonDetail";
 function App() {
   return (
     <LoginProvider>
@@ -12,7 +15,13 @@ function App() {
         <Routes>
           <Route index element={<Home />} />
           <Route path="about" element={<About />} />
-          <Route path="login" element={<Login/>} />
+          <Route path="login" element={<Login />} />
+
+          <Route path="people" element={<PrivateRouter />} />
+          <Route path="" element={<People />} />
+          <Route path=":id" element={<PersonDetail />} />
+
+          <Route path="*" element={<Navigate to="/" />} />
         </Routes>
       </BrowserRouter>
     </LoginProvider>
