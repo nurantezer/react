6f09aa43f2d2840b34bad4d19598285a24cc7e90
nurantezer <nurantezer68@gmail.com>
2023-03-15 14:13:19 +0300

    courses

--- 13_context-example/src/App.js
+++ 13_context-example/src/App.js
@@ -1,13 +1,17 @@
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import Navs from "./components/Navs";
 import LoginProvider from "./context/LoginProvider";
+import Home from "./pages/Home";
 
 function App() {
   return (
     <LoginProvider>
       <BrowserRouter>
         <Navs />
-        <Routes></Routes>
+        <Routes>
+          <Route index element={<Home />} />
+          
+        </Routes>
       </BrowserRouter>
     </LoginProvider>
   );
---
+++ 13_context-example/src/pages/Home.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+import Courses from '../components/Courses'
+
+const Home = () => {
+    return <Courses />
+}
+export default Home
